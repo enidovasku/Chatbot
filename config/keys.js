59ab7eac5f2c8feede09f6ff31825eb25003ce80
enido <enido.vasku@gmail.com>
2018-06-10 22:38:15 +0200

    Added Login - implement JWT -  Passport

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://enido:enido1234@ds153413.mlab.com:53413/devconnector"
+  mongoURI: "mongodb://enido:enido1234@ds153413.mlab.com:53413/devconnector",
+  secretOrKey: "secret"
 };
